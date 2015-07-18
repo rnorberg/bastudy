8077dfaf6199e0d52bedf32d76a8d4d161167c3e
rnorberg <robertcnorberg@gmail.com>
2015-07-18 17:19:47 -0400

    Added documentation for included data sets

---
+++ R/Data Documentation.R
@@ -0,0 +1,49 @@
+#' Traffic accidents at a reference site from 2003 to 2012
+#'
+#' A dataset containing the number of traffic accidents at an intersection
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item kabco. The combined number of accidents of several classes (k = fatality, a = incapacitating injury, b = non-incapacitating injury, c = possible injury, o = property damage only)
+#'   \item Max_AADT. Maximum Annual Average Daily Traffic
+#'   \item Min_AADT. Minimum Annual Average Daily Traffic
+#'   \item year. number of years of data included in the data set
+#' }
+#'
+#' @format A data frame with 318 rows and 4 variables
+#' @name Reference
+NULL
+
+
+#' Traffic accidents at an intersection prior to the installation of a traffic signal
+#'
+#' A dataset containing the number of traffic accidents at an intersection
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item kabco. The combined number of accidents of several classes (k = fatality, a = incapacitating injury, b = non-incapacitating injury, c = possible injury, o = property damage only)
+#'   \item Max_AADT. Maximum Annual Average Daily Traffic
+#'   \item Min_AADT. Minimum Annual Average Daily Traffic
+#'   \item year. number of years of data included in the data set
+#' }
+#'
+#' @format A data frame with 228 rows and 4 variables
+#' @name Before
+NULL
+
+
+#' Traffic accidents at an intersection after installation of a traffic signal
+#'
+#' A dataset containing the number of traffic accidents at an intersection
+#' The variables are as follows:
+#'
+#' \itemize{
+#'   \item kabco. The combined number of accidents of several classes (k = fatality, a = incapacitating injury, b = non-incapacitating injury, c = possible injury, o = property damage only)
+#'   \item Max_AADT. Maximum Annual Average Daily Traffic
+#'   \item Min_AADT. Minimum Annual Average Daily Traffic
+#'   \item year. number of years of data included in the data set
+#' }
+#'
+#' @format A data frame with 228 rows and 4 variables
+#' @name After
+NULL
