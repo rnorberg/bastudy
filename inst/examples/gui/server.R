9b971bea190c806b51250f010f5862f06879d9ea
rnorberg <robertcnorberg@gmail.com>
2015-07-21 07:21:54 -0400

    preliminary work on the GUI

--- inst/examples/gui/server.R
+++ inst/examples/gui/server.R
@@ -1,6 +1,57 @@
 library(shiny)
 
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output, clientData, session) {
+
+  output$selectDepVar <- renderUI({
+    if(is.null(input$before_trt)) return()
+    if(is.null(input$after_trt)) return()
+
+    wellPanel(
+    selectInput('depVarinput', label = 'Select Dependent Variable:',
+                choices = intersect(
+                  names(read.csv(input$before_trt$datapath, header=T, stringsAsFactors = F)),
+                  names(read.csv(input$after_trt$datapath, header=T, stringsAsFactors = F))
+                  )
+                )
+    )
+  })
+
+  output$selectOffestVar <- renderUI({
+    if(is.null(input$before_trt)) return()
+    if(is.null(input$after_trt)) return()
+
+    wellPanel(
+    selectInput('offsetVarinput', label = 'Select Offset Variable:',
+                choices = intersect(
+                  names(read.csv(input$before_trt$datapath, header=T, stringsAsFactors = F)),
+                  names(read.csv(input$after_trt$datapath, header=T, stringsAsFactors = F))
+                )
+    )
+    )
+  })
+
+  output$selectIndepVars <- renderUI({
+    if(is.null(input$before_trt)) return()
+    if(is.null(input$after_trt)) return()
+
+    wellPanel(
+    checkboxGroupInput('indepVarsinput', 'Select Independent Variables:',
+                       choices = intersect(
+                         names(read.csv(input$before_trt$datapath, header=T, stringsAsFactors = F)),
+                         names(read.csv(input$after_trt$datapath, header=T, stringsAsFactors = F))
+                       )
+    )
+    )
+  })
+
+  output$selectKeepVars <- renderUI({
+    if(is.null(input$indepVarsinput)) return()
+
+    checkboxGroupInput('keepVarsinput', 'Select any variables that should always be kept in the reference model:',
+                       choices = input$indepVarsinput
+                       )
+  })
+  output$Results <- renderPrint('your results here!')
 
 
 })
--- inst/examples/gui/ui.R
+++ inst/examples/gui/ui.R
@@ -1,7 +1,65 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  title = "Demo of ggExtra::ggMarginal()",
-  tags$head(includeCSS(file.path('www', 'style.css')))
+  titlePanel("GUI Helper for Package `bastudy`"),
+  fluidRow(
+    selectInput('analType', "Select Analysis Type",
+                choices = c('Naive', 'Comparison', 'Empirical Bayes'))
+  ),
 
+  fluidRow(
+    h4("Upload your data here"),
+    column(3,
+           wellPanel(
+             fileInput("before_trt", "Before", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+           )
+    ),
+    column(3,
+           wellPanel(
+             fileInput("after_trt", "After", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+           )
+    ),
+    column(6,
+           conditionalPanel(condition = "input.analType == 'Comparison'",
+                            column(6,
+                                   wellPanel(
+                                     fileInput("before_comp", "Comparison Data (before)", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+                                   )
+                            ),
+                            column(6,
+                                   wellPanel(
+                                     fileInput("after_comp", "Comparison Data (after)", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+                                   )
+                            )
+           ),
+           conditionalPanel(condition = "input.analType == 'Empirical Bayes'",
+                            column(6,
+                                   wellPanel(
+                                     fileInput("ref_data", "Reference Data", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
+                                   )
+                            )
+           )
+    )
+  ),
+
+  fluidRow(
+    h4("Specify dependent, independent, and offset variables"),
+    column(3,
+             uiOutput("selectDepVar")
+    ),
+    column(3,
+             uiOutput("selectOffestVar")
+    ),
+    column(3,
+             uiOutput("selectIndepVars")
+    )
+  ),
+  fluidRow(
+    column(3,
+           wellPanel(
+             uiOutput("selectKeepVars"),
+             sliderInput('conf_lev', "Select Confidence Level",
+                         min = 0, max = 1, value = 0.95, step = 0.01)
+           ))
+  )
 ))
